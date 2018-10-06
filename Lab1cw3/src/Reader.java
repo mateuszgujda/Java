7aca56d11da749f3551be42fdac5d695cc5a0fb2
mateuszgujda <43783866+mateuszgujda@users.noreply.github.com>
2018-10-06 15:35:15 +0200

    Lab1 Cw3
    Brak obsługiwanego wyjątku , dodano blok  try-catch

---
+++ Lab1cw3/src/Reader.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Reader{
+    public static void main(String[] argv){
+        InputStreamReader rd = new InputStreamReader(System.in);
+        BufferedReader bfr = new BufferedReader(rd);
+
+        System.out.print("Wpisz jakis tekst: ");
+        String text = null;
+        try {
+            text = bfr.readLine();
+        } catch (IOException e) {
+            System.out.print("Wyjatek" + e);
+        }
+        System.out.println("Wpisales: "+text); }
+}	  
